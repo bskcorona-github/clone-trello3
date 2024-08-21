dc31f154d0da69b5e6cb0db43e2057da609f8b12
bskcorona-github <kanemasa.tatsuro@gmail.com>
2024-08-21 21:30:09 +0900

    サイドバーやりかけ

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+const OrganizationIdPage = () => {
+  return <div>OrganizationId Page</div>;
+};
+
+export default OrganizationIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,38 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// 保護されたルートを明示的に指定 ("/"を除外)
+// 公開ルートを指定（サインイン、サインアップページを含む）
+const publicRoutes = ["/", "/sign-in", "/sign-up", "/api/webhook"];
 const isProtectedRoute = createRouteMatcher(["/protected"]);
 
 export default clerkMiddleware((auth, req) => {
   const userId = auth().userId;
-  const isSignInPage = req.nextUrl.pathname === "/sign-in";
-  const isRootPage = req.nextUrl.pathname === "/";
+  const orgId = auth().orgId;
+  const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
 
-  // 認証されていない場合、サインインページ以外にアクセスするとサインインページへリダイレクト
-  if (!userId && !isSignInPage && !isRootPage) {
+  // 認証されていない場合、公開ルート以外にアクセスするとサインインページへリダイレクト
+  if (!userId && !isPublicRoute) {
     return auth().redirectToSignIn({
       returnBackUrl: req.url,
     });
+  }
+
+  // 認証済みで公開ルートにアクセスしようとしている場合
+  if (userId && isPublicRoute) {
+    let path = "/select-org";
+
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // 認証済みだが組織IDが設定されていない場合、組織選択ページにリダイレクト
+  if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
   }
 
   // 保護されたルートにアクセスしようとしている場合
